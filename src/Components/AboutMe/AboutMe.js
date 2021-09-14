58fddcb55ad50c5cf6d4b6c42efaa954a2b23fca
Agnieszka Polińska <agapoli@gmail.com>
2021-09-14 21:31:30 +0200

    Second version

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,3 +1,4 @@
+import MyPhoto from '../../images/about-me.jpg';
 import './AboutMe.scss';
 
 const AboutMe = () => {
@@ -5,6 +6,7 @@ const AboutMe = () => {
         <div className="about-me">
             <h2 className="main-content__header">O mnie</h2>
             <p className="main-content__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+            <div className="about-me__image-box"><img src={MyPhoto} alt="Me and my dogs"/></div>
         </div>
     );
 }
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
         <div className="contact">
             <h2 className="main-content__header">Kontakt do mnie</h2>
             <p className="main-content__text">
-                Dojeżdżam do pacjentów na terenie Wrocławia i okolic. W celu ustalenia szczegółów proszę o kontakt telefoniczny.
+                Dojeżdżam do pacjentów na terenie Wrocławia i okolic. W&nbsp;celu ustalenia szczegółów proszę o kontakt telefoniczny.
             </p>
             <a href="tel:+48661110622" className="main-content__text contact__link"><FaPhoneAlt /> 661 110 622</a>
         </div>
